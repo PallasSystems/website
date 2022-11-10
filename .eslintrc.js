1bb2cb0cff6d6cf0398c9cfa3ff88e969f2c0ec3
Stephen C <stephen.crocker@pallas.uk>
2022-11-10 22:08:43 +0000

    Added some more react specific configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,13 @@ module.exports = {
       "import/extensions": [
         ".js",
         ".jsx"
-      ]
+      ],
+      "react": {
+        "createClass": "createReactClass", // Regex for Component Factory to use default to "createReactClass"
+        "pragma": "React",  // Pragma to use, default to "React"
+        "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+        "version": "18",
+        "flowVersion": "0.53" // Flow version
+      }
     }
 }

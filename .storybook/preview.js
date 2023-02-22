6623639af9ff9cb198622c5dae36e3a5e2ee33f2
Stephen C <stephen.crocker@pallas.uk>
2023-02-22 14:18:46 +0000

    Added storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/bootstrap.pallas.css';
+
+import { addDecorator } from "@storybook/react";
+import { MemoryRouter } from "react-router";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter initialEntries={['/']}>
+      <Story />
+    </MemoryRouter>
+  ),
+];

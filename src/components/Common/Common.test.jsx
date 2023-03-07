50c1cb481d5a72c2fa12cd0018f6f399231494f1
Stephen C <stephen.crocker@pallas.uk>
2023-03-07 15:21:01 +0000

    Complete Set of Tests to confirm the SVG's take the values we supply

---
+++ src/components/Common/Common.test.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+
+import { AutomationSVG, InfrastructureAsCodeSVG, PallasSVG } from './Common.components';
+
+const imageProps = {id: "identifier", className: "bob", height: "5rem", width: "5rem"};
+
+test("Blank Automation", () => {
+  render(<AutomationSVG />);
+
+  const result = screen.getByTitle("Automation");
+  expect(result).toBeValid();
+});
+
+test("Blank InfrastructureAsCode", () => {
+  render(<InfrastructureAsCodeSVG />);
+  
+  const result = screen.getByTitle("Infrastructure as Code");
+  expect(result).toBeValid();
+});
+
+test("Blank Pallas Logo", () => {
+  render(<PallasSVG />);
+  
+  const result = screen.getByTitle("Pallas Logo");
+  expect(result).toBeValid();
+});
+
+test("Automation with properties", () => {
+  render(<AutomationSVG {...imageProps} />);
+
+  const result = screen.getByTitle("Automation");
+  expect(result).toBeValid();
+
+  expect(result.parentElement).toHaveAttribute('id', imageProps.id);
+  expect(result.parentElement).toHaveAttribute('height', imageProps.height);
+  expect(result.parentElement).toHaveAttribute('width', imageProps.width);
+});
+  
+test("InfrastructureAsCode with properties", () => {
+  render(<InfrastructureAsCodeSVG {...imageProps} />);
+
+  const result = screen.getByTitle("Infrastructure as Code");
+  expect(result).toBeValid();
+
+  expect(result.parentElement).toHaveAttribute('id', imageProps.id);
+  expect(result.parentElement).toHaveAttribute('height', imageProps.height);
+  expect(result.parentElement).toHaveAttribute('width', imageProps.width);
+});
+  
+test("Pallas Logo with properties", () => {
+  render(<PallasSVG {...imageProps} />);
+    
+  const result = screen.getByTitle("Pallas Logo");
+  expect(result).toBeValid();
+
+  expect(result.parentElement).toHaveAttribute('id', imageProps.id);
+  expect(result.parentElement).toHaveAttribute('height', imageProps.height);
+  expect(result.parentElement).toHaveAttribute('width', imageProps.width);
+});
+
--- src/components/SCMIcon/SCMIcon.test.tsx
+++ src/components/SCMIcon/SCMIcon.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { queries, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
 import {SCMIcon} from './SCMIcon.components';
@@ -12,12 +12,14 @@ test("Valid GitHub Credentials", () => {
 
 test("Blank GitHub Credentials", () => {
   render(<SCMIcon project="" repository="" />);
+  
   const link = screen.getByRole("link");
   expect(link).toHaveAttribute('href', '');
 });
 
 test("Valid Alternative Credentials", () => {
   render(<SCMIcon project="proj" repository="repo" endpoint="https://bitbucket.org/" />);
+  
   const link = screen.getByRole("link");
   expect(link).toHaveAttribute('href', 'https://bitbucket.org/proj/repo');
-});queries
+});

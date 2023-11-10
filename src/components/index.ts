25f1864daa98bf635caf12795e15383e002a1e43
Stephen Crocker <stephen.crocker@pallas.uk>
2023-11-10 10:55:01 +0000

    Removed them from the core

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,26 +1,9 @@
-export { AutomationSVG, InfrastructureAsCodeSVG, PallasSVG } from './Common/index';
-export type { ImageProperty } from './Common/index';
+export { AutomationSVG, InfrastructureAsCodeSVG } from "./Common/index";
+export type { ImageProperty } from "./Common/index";
 
 // All of these items are linked to the Home page
-export { BannerCarousel } from './BannerCarousel/index';
-export type { BannerCarouselItemProperty } from './BannerCarousel/index';
+export { BannerCarousel } from "./BannerCarousel/index";
+export type { BannerCarouselItemProperty } from "./BannerCarousel/index";
 
-export { Featurettes } from './Featurettes/index';
-export type { FeaturetteProperty } from './Featurettes/index';
-
-// All of these are linked to the Header for the page
-export { BannerNavBar, BannerNavBarDefaultData } from './BannerNavBar/index';
-export type { NavbarProperty, NavbarLinkProperty } from './BannerNavBar/index';
-
-// All of these are linked to the Footer, which is used on multiple pages.
-export { Footer, FooterDefaultData } from './Footer/index';
-export type { FooterProperties } from './Footer/index';
-
-export { ContactSection } from './ContactSection/index';
-export type { ContactProperties, AddressSectionProperties } from './ContactSection/index';
-
-export { SocialMediaSection } from './SocialMediaSection/index';
-export type { SocialMediaProperties } from './SocialMediaSection/index';
-
-export { SCMIcon } from './SCMIcon/index';
-export type { SCMProperties } from './SCMIcon/index';
+export { Featurettes } from "./Featurettes/index";
+export type { FeaturetteProperty } from "./Featurettes/index";

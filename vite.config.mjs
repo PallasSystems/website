c6cd37518581c4b099a71b0911bb5fb3f10f9a11
Stephen Crocker <stephen.crocker@pallas.uk>
2024-01-29 00:50:18 +0100

    Fixed the domain@

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://pallas.uk/',
+  base: 'https://www.pallas.uk/',
   plugins: [react()],
   test: {
     coverage: {

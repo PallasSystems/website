d4ed2c925134bd2db23d081078a62c99d654012b
Stephen Crocker <stephen.crocker@pallas.uk>
2024-01-28 20:32:05 +0100

    Updated to Vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app"],
-  "framework": {
-    name: "@storybook/react-webpack5",
-    options: {}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  core: {
+    builder: '@storybook/builder-vite',
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,33 @@
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import {
-  HomePage,
-  MissingPage,
-  ServicesPage,
-  ServicePageData,
-  TeamPage,
-  TeamBioPage,
-  TeamBiosData,
-} from "./pages";
-import { FooterData, NavBarData } from "./App.data";
+import { HomePage, MissingPage, ServicesPage, ServicePageData, TeamPage, TeamBioPage, TeamBiosData } from './pages';
+import { FooterData, NavBarData } from './App.data';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path={""} element={<HomePage  footerProps={FooterData} navBarProps={NavBarData} />} />
+        <Route path={''} element={<HomePage footerProps={FooterData} navBarProps={NavBarData} />} />
 
         <Route
           path={ServicePageData.link}
           element={<ServicesPage footerProps={FooterData} navBarProps={NavBarData} serviceProps={ServicePageData} />}
         />
-        { ServicePageData.items ? ServicePageData.items.map((service) => {
-          return (
-            <Route
-              key={service.title}
-              path={service.link}
-              element={<ServicesPage footerProps={FooterData} navBarProps={NavBarData} serviceProps={service} />}
-            />
-          );
-        }) : null }
-        <Route path={"/team"} element={<TeamPage bios={TeamBiosData} footerProps={FooterData} navBarProps={NavBarData} />} />
+        {ServicePageData.items
+          ? ServicePageData.items.map((service) => {
+              return (
+                <Route
+                  key={service.title}
+                  path={service.link}
+                  element={<ServicesPage footerProps={FooterData} navBarProps={NavBarData} serviceProps={service} />}
+                />
+              );
+            })
+          : null}
+        <Route
+          path={'/team'}
+          element={<TeamPage bios={TeamBiosData} footerProps={FooterData} navBarProps={NavBarData} />}
+        />
         {TeamBiosData.map((bio) => {
           return (
             <Route
@@ -41,7 +37,7 @@ function App() {
             />
           );
         })}
-        <Route path={"/404"} element={<MissingPage />} />
+        <Route path={'/404'} element={<MissingPage />} />
       </Routes>
     </HashRouter>
   );

d4f4c92f75dd606057441c269db849cc99e840e5
Stephen Crocker <stephen.crocker@pallas.uk>
2024-01-30 21:50:13 +0100

    Updated the website to include everything

--- src/pages/Services/devsecops/DevSecOps.data.ts
+++ src/pages/Services/devsecops/DevSecOps.data.ts
@@ -1,75 +1,84 @@
 import { ServiceDetailProperties } from '../Services.types';
-import { ServiceAutomationData, ServiceIaaCData, ServiceSupportData} from '../cloud/index'
+import { ServiceAutomationData, ServiceIaaCData, ServiceSupportData } from '../cloud/index';
+
+import devSecopsImg from './devsecops.svg';
 
 export const ServiceCIData: ServiceDetailProperties = {
-    title: "Continuous Integration",
-    link: "/services/agile",
-    thumbnail: { 
-        title: "Continuous Integration",
-        link: "/services/agile",
-        description: ["We can integrate CI pipelines to ensure code quality, to stop breaking changes and track technical debt"],    
-        img: { 
-            id:"Container.Description.Row.Col.Img", 
-            src: "Fail Fast", 
-            height:"9rem", 
-            width:"9rem" 
-        },
+  title: 'Continuous Integration',
+  link: '/services/agile',
+  thumbnail: {
+    title: 'Continuous Integration',
+    link: '/services/agile',
+    description: [
+      'We can integrate CI pipelines to ensure code quality, to stop breaking changes and track technical debt',
+    ],
+    img: {
+      id: 'Container.Description.Row.Col.Img',
+      src: 'Fail Fast',
+      height: '9rem',
+      width: '9rem',
     },
-    description: ["We can integrate CI pipelines to ensure code quality, to stop breaking changes and track technical debt"]
+  },
+  description: [
+    'We can integrate CI pipelines to ensure code quality, to stop breaking changes and track technical debt',
+  ],
 };
 
 export const ServiceCDData: ServiceDetailProperties = {
-    title: "Continuous Deployment",
-    link: "/services/agile",
-    thumbnail: { 
-        title: "Continuous Deployment",
-        link: "/services/agile",
-        description: ["We will automate your deployments to reduce the time from merging a change to getting it into production"],
-        img: {
-            id:"Container.Description.Row.Col.Img", 
-            src: "Fail Fast", 
-            height:"9rem", 
-            width:"9rem" 
-        },
+  title: 'Continuous Deployment',
+  link: '/services/agile',
+  thumbnail: {
+    title: 'Continuous Deployment',
+    link: '/services/agile',
+    description: [
+      'We will automate your deployments to reduce the time from merging a change to getting it into production',
+    ],
+    img: {
+      id: 'Container.Description.Row.Col.Img',
+      src: 'Fail Fast',
+      height: '9rem',
+      width: '9rem',
     },
-    description: ["We will automate your deployments to reduce the time from merging a change to getting it into production"]
+  },
+  description: [
+    'We will automate your deployments to reduce the time from merging a change to getting it into production',
+  ],
 };
 
 export const ServiceTestingData: ServiceDetailProperties = {
-    title: "Testing",
-    link: "/services/agile",
-    thumbnail: { 
-        title:'Testing', 
-        link:"/services/agile", 
-        description: [""], 
-        img: {
-            id:"Container.Description.Row.Col.Img", 
-            src: "Fail Fast", 
-            height:"9rem", 
-            width:"9rem" 
-        }
+  title: 'Testing',
+  link: '/services/agile',
+  thumbnail: {
+    title: 'Testing',
+    link: '/services/agile',
+    description: [''],
+    img: {
+      id: 'Container.Description.Row.Col.Img',
+      src: 'Fail Fast',
+      height: '9rem',
+      width: '9rem',
     },
-    description: ["We use a mixture of unit, integration and system testing to ensure software meets your use cases"]
+  },
+  description: ['We use a mixture of unit, integration and system testing to ensure software meets your use cases'],
 };
 
-
 export const ServiceDevSecOpsData = {
-    title: "What can we do to help?",
-    link: "/services/devsecops",
-    img: { id:"Container.Description.Row.Col.Img", src: './images/services/devsecops.svg' },
+  title: 'What can we do to help?',
+  link: '/services/devsecops',
+  img: { id: 'Container.Description.Row.Col.Img', src: devSecopsImg },
+  description: [
+    'We use DevSecOps to shorten the feedback loop, to be able to take your idea&apos;s and bring them into production quickly. DevSecOps also provides a bevy of tools to track quality',
+    'We have a number of Continuous Integration (CI) pipelines for various software programming languages looking to enable various assurance activities on our software. We look to automate new requirements into existing pipelines to ensure all customers can benefit from the enhancements. We have automated issue tracking, release management, automated testing and code analysis activities for various development stacks and languages.',
+    'We have developed various Continuous Deployment (CD) pipelines, we have used Puppet and Ansible to deploy software to physical servers and deployed packages to serverless platforms (e.g. AWS Lambda). While we can provide full automated Ci/CD pipelines we can also create gated processes to allow for assurance review.',
+    'Pallas Systems collects metrics from all CI/CD pipelines, this information is used to inform development of the software. Allowing us to focus on issues appropriate for the software development approach.',
+  ],
+  thumbnail: {
+    title: 'DevSecOps',
+    img: { id: 'BannerNavBar.Brand.Link.Logo.SVG', src: './cloud/cloud_small.svg', height: '9rem', width: '9rem' },
+    link: '/services/devsecops',
     description: [
-                "We use DevSecOps to shorten the feedback loop, to be able to take your idea&apos;s and bring them into production quickly. DevSecOps also provides a bevy of tools to track quality",
-                "We have a number of Continuous Integration (CI) pipelines for various software programming languages looking to enable various assurance activities on our software. We look to automate new requirements into existing pipelines to ensure all customers can benefit from the enhancements. We have automated issue tracking, release management, automated testing and code analysis activities for various development stacks and languages.",
-                "We have developed various Continuous Deployment (CD) pipelines, we have used Puppet and Ansible to deploy software to physical servers and deployed packages to serverless platforms (e.g. AWS Lambda). While we can provide full automated Ci/CD pipelines we can also create gated processes to allow for assurance review.",
-                "Pallas Systems collects metrics from all CI/CD pipelines, this information is used to inform development of the software. Allowing us to focus on issues appropriate for the software development approach."
+      'Pallas Systems uses DevSecOps to enable us to quickly respond to your requests and ensure the integrity of your software and services',
     ],
-    thumbnail: {
-        title:'DevSecOps',
-        img: { id:"BannerNavBar.Brand.Link.Logo.SVG", src: './cloud/cloud_small.svg', height:"9rem", width:"9rem" },
-        link: '/services/devsecops',
-        description:[
-                'Pallas Systems uses DevSecOps to enable us to quickly respond to your requests and ensure the integrity of your software and services'
-        ]
-    },
-    items: [ ServiceAutomationData, ServiceCIData, ServiceCDData, ServiceIaaCData, ServiceSupportData, ServiceTestingData ]
+  },
+  items: [ServiceAutomationData, ServiceCIData, ServiceCDData, ServiceIaaCData, ServiceSupportData, ServiceTestingData],
 };
--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://www.pallas.uk/',
+  assetsInclude: ['src/**/*.svg', 'src/**/*.jpg'],
   plugins: [react()],
   test: {
     coverage: {
